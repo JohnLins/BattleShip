ec030516b6eebb89bc38b2f0445408af7e090eb9
John Lins <johnpaullins@gmail.com>
2022-11-21 20:56:31 -0800

    All done

--- BattleShip.ino
+++ BattleShip.ino
@@ -10,7 +10,8 @@ CRGB leds[NUM_LEDS];
 
 #define X_JOY A0
 #define Y_JOY A1
-#define SW_JOY 6
+#define SW_JOY_LEFT 6
+#define SW_JOY_RIGHT 12
 
 
 
@@ -21,7 +22,7 @@ int last_index;
 CRGB last_value_inactive;
 int last_index_inactive;
 
-//////////////////////
+
 #define dataPin 8
 #define clockPin 10
 #define latchPin 9
@@ -30,16 +31,16 @@ int last_index_inactive;
 #define digit2pin 4
 #define digit3pin 5
 int digit2segments []={
-0b00111111 , // d i g i t 0
-0b00000110 , // d i g i t 1
-0b01011011 , // d i g i t 2
-0b01001111 , // d i g i t 3
-0b01100110 , // d i g i t 4
-0b01101101 , // d i g i t 5
-0b01111101 , // d i g i t 6
-0b00000111 , // d i g i t 7
-0b01111111, // d i g i t 8
-0b01101111 }; // d i g i t 9
+0b00111111 , //0
+0b00000110 , //1
+0b01011011 , //2
+0b01001111 , //3
+0b01100110 , //4
+0b01101101 , //5
+0b01111101 , //6
+0b00000111 , //7
+0b01111111,  //8
+0b01101111 }; //9
 
 
 int cur_x = 0;
@@ -136,11 +137,11 @@ typedef struct Cord {
   CRGB color;
 } Cord;
 typedef struct Player {
-  int ships[NUM_SHIP_PIX];// = (int *)malloc(sizeof(int)*6);
-  int score;// = 0;
+  int ships[NUM_SHIP_PIX];
+  int score;
   //Cord *shots;// = (Cord *)malloc(0);
   Cord shots[20];
-  int num_shots;// = 0;
+  int num_shots;
 } Player;
 
 Player left = (Player){{xy_to_index(0,1, LEFT),
@@ -165,28 +166,29 @@ Player left = (Player){{xy_to_index(0,1, LEFT),
                           xy_to_index(14,12, LEFT),
                           xy_to_index(14,13, LEFT),
                           xy_to_index(14,14, LEFT)}, 0, /*(Cord *)malloc(0)*/{0,0}, 0};
-Player right = (Player){{xy_to_index(0,6, RIGHT),
-xy_to_index(1,6, RIGHT),
-xy_to_index(2,6, RIGHT),
-xy_to_index(1,0, RIGHT),
-xy_to_index(1,1, RIGHT),
-xy_to_index(4,13, RIGHT),
-xy_to_index(5,13, RIGHT),
-xy_to_index(6,13, RIGHT),
-xy_to_index(5,12, RIGHT),
-xy_to_index(5,11, RIGHT),
-xy_to_index(8,9, RIGHT),
-xy_to_index(8,10, RIGHT),
-xy_to_index(8,11, RIGHT),
-xy_to_index(8,12, RIGHT),
-xy_to_index(12,11, RIGHT),
-xy_to_index(12,12, RIGHT),
-xy_to_index(12,13, RIGHT),
-xy_to_index(10,2, RIGHT),
-xy_to_index(11,2, RIGHT),
-xy_to_index(12,2, RIGHT),
-xy_to_index(13,2, RIGHT),
-xy_to_index(14,2, RIGHT)}, 0, /*(Cord *)malloc(0)*/{0,0}, 0};
+Player right = (Player){{ xy_to_index(0,6, RIGHT),
+                          xy_to_index(1,6, RIGHT),
+                          xy_to_index(2,6, RIGHT),
+                          xy_to_index(1,0, RIGHT),
+                          xy_to_index(1,1, RIGHT),
+                          xy_to_index(4,13, RIGHT),
+                          xy_to_index(5,13, RIGHT),
+                          xy_to_index(6,13, RIGHT),
+                          xy_to_index(5,12, RIGHT),
+                          xy_to_index(5,11, RIGHT),
+                          xy_to_index(8,9, RIGHT),
+                          xy_to_index(8,10, RIGHT),
+                          xy_to_index(8,11, RIGHT),
+                          xy_to_index(8,12, RIGHT),
+                          xy_to_index(12,11, RIGHT),
+                          xy_to_index(12,12, RIGHT),
+                          xy_to_index(12,13, RIGHT),
+                          xy_to_index(10,2, RIGHT),
+                          xy_to_index(11,2, RIGHT),
+                          xy_to_index(12,2, RIGHT),
+                          xy_to_index(13,2, RIGHT),
+                          xy_to_index(14,2, RIGHT)}, 
+0, /*(Cord *)malloc(0)*/{0,0}, 0};
 
 void plus_score(bool active){
   if(active == LEFT){
@@ -244,20 +246,23 @@ void setup(){
 
   
  
-  pinMode(buzzer, OUTPUT); // Set buzzer - pin 9 as an output
+  pinMode(buzzer, OUTPUT); 
 
 
-  pinMode(SW_JOY, INPUT);
-  digitalWrite(SW_JOY, HIGH);
+  pinMode(SW_JOY_LEFT, INPUT);
+  digitalWrite(SW_JOY_LEFT, HIGH);
+
+  pinMode(SW_JOY_RIGHT, INPUT);
+  digitalWrite(SW_JOY_RIGHT, HIGH);
   
   Serial.begin(9600);
 
   
   FastLED.addLeds<WS2812B, LED_PIN, RGB>(leds, NUM_LEDS);
-  FastLED.setBrightness(50); //Number 0-255
+  FastLED.setBrightness(50); 
   FastLED.clear();
 
-  //////////////////
+
   pinMode ( latchPin , OUTPUT );
   pinMode ( dataPin , OUTPUT );
   pinMode ( clockPin , OUTPUT );
@@ -265,9 +270,6 @@ void setup(){
   pinMode ( digit1pin , OUTPUT );
   pinMode ( digit2pin , OUTPUT );
   pinMode ( digit3pin , OUTPUT );
-
-
-
 
 
 }
@@ -298,8 +300,8 @@ if(active == LEFT){
   for(int i = 0; i < NUM_SHIP_PIX; i++){
     leds[right.ships[i]] = 0xFFA500;
   }
-  for(int i = 0; i < left.num_shots/*sizeof(right.shots)/sizeof(Cord)*/; i++){
-    leds[left.shots[i].index]/*, leds[left.shots[i].index+256]*/ = left.shots[i].color;
+  for(int i = 0; i < left.num_shots; i++){
+    leds[left.shots[i].index] = left.shots[i].color;
   } 
   
 } else {
@@ -312,8 +314,8 @@ if(active == LEFT){
   for(int i = 0; i < NUM_SHIP_PIX; i++){
     leds[left.ships[i]] = 0xFFA500;
   }
-  for(int i = 0; i < right.num_shots/*sizeof(right.shots)/sizeof(Cord)*/; i++){
-    leds[right.shots[i].index]/*, leds[right.shots[i].index - 256]*/  = right.shots[i].color;
+  for(int i = 0; i < right.num_shots; i++){
+    leds[right.shots[i].index] = right.shots[i].color;
   } 
   
 }
@@ -372,15 +374,14 @@ if(active == LEFT){
   
   joy.x = analogRead(X_JOY);
   joy.y = analogRead(Y_JOY);
-  joy.fire = !digitalRead(SW_JOY);
+  if(active == LEFT){
+      joy.fire = !digitalRead(SW_JOY_LEFT);
+  } else {
+    joy.fire = !digitalRead(SW_JOY_RIGHT);
+  }
 
 
-//     for(int i = 0; i < 30000; i++){
-//    
-//      
-//      tone(buzzer,500 + (int)(i/500));
-//      
-//     }
+
     if(false){
      for(int i = 0; i < 5000; i++){
     
@@ -395,15 +396,10 @@ if(active == LEFT){
     }
 
 
-    if(joy.y > 600 && cur_y > 0) {cur_y --; /*render[cursor.x][cursor.y-1] = already_on; */}
-    if(joy.y < 400 && cur_y < 15) {cur_y ++; /*render[cursor.x][cursor.y+1] = already_on; */}
-    if(joy.x > 600 && cur_x < 15) {cur_x ++; /*render[cursor.x-1][cursor.y] = already_on; */}
-    if(joy.x < 400 && cur_x > 0) {cur_x --; /*render[cursor.x+1][cursor.y] = already_on; */}
-//  
+    if(joy.y > 600 && cur_y > 0) {cur_y --; }
+    if(joy.y < 400 && cur_y < 15) {cur_y ++; }
+    if(joy.x > 600 && cur_x < 15) {cur_x ++; }
+    if(joy.x < 400 && cur_x > 0) {cur_x --; }
 
-  
- 
-
-delay(100);
  
 }
